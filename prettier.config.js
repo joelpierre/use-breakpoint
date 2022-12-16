c123715b3c6e76e2add808bc73ea8d2acc1f2418
Joel Pierre <jpp@jppdesigns.co.uk>
2022-12-16 12:10:43 +0000

    chore: remove yarn cache

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  bracketSpacing: true,
+  singleQuote: true,
+  semi: true,
+  printWidth: 80,
+  tabWidth: 2,
+  trailingComma: 'all',
+  arrowParens: 'avoid',
+  singleAttributePerLine: true,
+};
