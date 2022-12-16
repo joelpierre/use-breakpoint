b650d03c5b00fc5666d6bce6820a2bad61116b36
Joel Pierre <jpp@jppdesigns.co.uk>
2022-12-16 12:37:27 +0000

    chore: Add commitlint

---
+++ commitlint.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat",
+        "fix",
+        "docs",
+        "style",
+        "refactor",
+        "test",
+        "revert",
+        "nfr",
+        "release",
+        "build",
+        "chore",
+        "ci",
+        "fix",
+        "perf",
+      ],
+    ],
+  },
+};
